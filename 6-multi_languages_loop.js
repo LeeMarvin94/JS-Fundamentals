f5e8a0289e6efcfd528c3f26128aebb53c0ef016
ONDO ABAGHE Rooly Marvin <rollymarvinondoabaghe@gmail.com>
2025-09-14 20:44:55 +0100

    Adding the task-7

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Array of strings
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through the array and print each element
+for (const line of lines) {
+  console.log(line);
+}
+
