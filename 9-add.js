b4c28ed3c4faf616dde50627f857f0401bc0b8d9
ONDO ABAGHE Rooly Marvin <rollymarvinondoabaghe@gmail.com>
2025-09-14 20:49:42 +0100

    Adding the task-10

---
+++ 9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Define the add function
+function add(a, b) {
+  return a + b;
+}
+
+// Get arguments and convert to integers
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+// Print the result
+console.log(add(num1, num2));
+
